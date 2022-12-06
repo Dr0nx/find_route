733f691373f8d6e2bc7a5cb91a29f70b814e56b1
Andrey Bozhkov <kovbozh@gmail.com>
2022-12-06 10:41:35 +0300

    Update urls.py

--- cities/urls.py
+++ cities/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from cities.views import *
 
 urlpatterns = [
-    path('', home, name='home'),
     # path('<int:pk>', home, name='home'),
-    path('detail/<int:pk>', CityDetailView.as_view(), name='detail'),
+    # path('', home, name='home'),
+    path('', CityListView.as_view(), name='home'),
+    path('create/', CityCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
 ]
