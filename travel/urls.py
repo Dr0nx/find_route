9c497816e4ff21d3ce1fcf4f2d1bc8c47e9f2770
Andrey Bozhkov <kovbozh@gmail.com>
2022-12-07 20:35:36 +0300

    Update urls.py

--- travel/urls.py
+++ travel/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('cities/', include(('cities.urls', 'cities')))
+    path('cities/', include(('cities.urls', 'cities'))),
+    path('trains/', include(('trains.urls', 'trains'))),
 ]
